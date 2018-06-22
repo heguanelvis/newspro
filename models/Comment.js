59d5e1ad8ace5b455eb4e9450bc6b951b55871de
Guan <heguanelvis@gmail.com>
2018-06-22 15:16:44 -0500

    connected to local mongo db

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let CommentSchema = new Schema({
+    name: {
+        type: String
+    },
+    body: {
+        type: String,
+        required: true
+    }
+});
+
+let Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+
+app.use(express.static("public"));
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Local Mongo Database
+var db = 'mongodb://localhost/CrazyNewsifier';
+
+mongoose.connect(db, function (err) {
+
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('mongoose connection is sucessful');
+    }
+
+});
+
+//////////////////////////////////////////Routes///////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
