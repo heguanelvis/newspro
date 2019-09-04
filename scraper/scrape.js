ddc0c41b0fd822bcd9db8d529d8b84ce0aabbfd9
heguanelvis <heguanelvis@gmail.com>
2019-09-03 20:06:24 -0400

    added try catch block to resolve potential errors coming from change of NYT site

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -10,31 +10,33 @@ const scrape = newsUrl => {
 
       $("div.css-4jyr1y").each((i, element) => {
         if (i >= 10) return;
-
-        const url = `https://www.nytimes.com${$(element)
-          .find("a")
-          .attr("href")}`;
-        const headline = $(element)
-          .find("a")
-          .find("h2")
-          .text()
-          .trim();
-        const summary = $(element)
-          .find("a")
-          .find("p.e1xfvim31")
-          .text()
-          .trim();
-        const img = $(element)
-          .find("div.css-79elbk")
-          .find("img")
-          .attr("src")
-          .replace(
-            "thumbWide.jpg?quality=75&auto=webp&disable=upscale",
-            "threeByTwoMediumAt2X.jpg"
-          );
-
-        const news = { url, headline, summary, img, tempId: uuidv4() };
-        store.push(news);
+        try {
+          const url = `https://www.nytimes.com${$(element)
+            .find("a")
+            .attr("href")}`;
+          const headline = $(element)
+            .find("a")
+            .find("h2")
+            .text()
+            .trim();
+          const summary = $(element)
+            .find("a")
+            .find("p.e1xfvim31")
+            .text()
+            .trim();
+          const img = $(element)
+            .find("div.css-79elbk")
+            .find("img")
+            .attr("src")
+            .replace(
+              "thumbWide.jpg?quality=75&auto=webp&disable=upscale",
+              "threeByTwoMediumAt2X.jpg"
+            );
+          const news = { url, headline, summary, img, tempId: uuidv4() };
+          store.push(news);
+        } catch (error) {
+          console.log("There might be some recent changes with NYT");
+        }
       });
 
       resolve(store);
